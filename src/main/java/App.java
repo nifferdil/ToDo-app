ebdc9b721556bb8bde88ec318166f3f0f29204e3
nifferdil <nifferdil@gmail.com>
2015-08-25 15:47:49 -0700

    add tasks but also add an empty string

---
+++ src/main/java/App.java
@@ -0,0 +1,88 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import static spark.Spark.*;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+public class App {
+  public static void main(String[] args){
+    //staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      model.put("tasks", request.session().attribute("tasks"));
+
+      return new ModelAndView(model, layout);
+   }, new VelocityTemplateEngine());
+
+   get("/categories/new", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/category-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/categories", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("name");
+      Category newCategory = new Category(name);
+      newCategory.save();
+      model.put("category", newCategory);
+      model.put("template", "templates/success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/categories", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("categories", Category.all());
+      model.put("template", "templates/categories.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/categories/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      // Id parameter is key from the database when we use the save method
+      Category category = Category.find(Integer.parseInt(request.params(":id")));
+      model.put("category", category);
+      model.put("template", "templates/category.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/categories/:id/tasks/new", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Category category = Category.find(Integer.parseInt(request.params(":id")));
+      model.put("category", category);
+      String description = request.queryParams("description");
+      Task newTask = new Task(description, category.getId());
+      newTask.save();
+      model.put("template", "templates/category-tasks-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/tasks", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Category category = Category.find(Integer.parseInt(request.queryParams("categoryId")));
+      String description = request.queryParams("description");
+      //System.out.println(description);
+      Task newTask = new Task(description, category.getId());
+      newTask.save();
+      model.put("category", category);
+      model.put("template", "templates/category.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+  //   get("/tasks", (request, response) -> {
+  //     HashMap<String, Object> model = new HashMap<String, Object>();
+  //     model.put("tasks", Task.all());
+  //     model.put("template", "templates/category.vtl");
+  //     return new ModelAndView(model, layout);
+  //  }, new VelocityTemplateEngine());
+
+
+
+ }
+
+}
---
+++ src/main/java/Category.java
@@ -0,0 +1,67 @@
+import java.util.List;
+import org.sql2o.*;
+import java.util.Arrays;
+
+public class Category {
+  private int id;
+  private String name;
+
+  public int getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Category(String name) {
+    this.name = name;
+  }
+
+  public static List<Category> all() {
+    String sql = "SELECT id, name FROM categories";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Category.class);
+    }
+  }
+
+  @Override
+  public boolean equals(Object otherCategory){
+    if (!(otherCategory instanceof Category)) {
+      return false;
+    } else {
+      Category newCategory = (Category) otherCategory;
+      return this.getName().equals(newCategory.getName());
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO categories(name) VALUES (:name)";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("name", this.name)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  public static Category find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM categories WHERE id=:id";
+      Category Category = con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Category.class);
+      return Category;
+    }
+  }
+
+  public List<Task> getTasks() {
+  try(Connection con = DB.sql2o.open()) {
+    String sql = "SELECT * FROM tasks WHERE categoryId=:id";
+    return con.createQuery(sql)
+      .addParameter("id", id)
+      .executeAndFetch(Task.class);
+  }
+}
+
+}
